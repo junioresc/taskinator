032f98442eb6e817faff10f352af7fd7e5aae6e8
Junioresc <junioresc1092@gmail.com>
2020-06-25 14:06:18 -0400

    created div to hold list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Creates list item
     var listItemEl= document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list
+    var taskInfoEl = document.createElement("div");
+    // Give it a class name
+    taskInfoEl.className = "task-info";
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
-console.log(buttonEl);
